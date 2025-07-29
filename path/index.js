3f27b98d05c5a9cecd29873b1ccbe4d6c2d1a9e2
Subhamstar <mr.subhamdas999@gmail.com>
2025-07-29 22:52:52 +0530

    node js path

--- path/index.js
+++ path/index.js
@@ -1,3 +1,4 @@
+const { log } = require("console");
 const path=require("path");
 
 // console.log(path.sep);  //          \-windows, /-mac,linux
@@ -6,4 +7,29 @@ const path=require("path");
 // console.log(process.env.PATH);
 
 const currFilepath=__filename;
-console.log(currFilepath);
+console.log('path -> : ',currFilepath);
+
+// current file name
+let basename=path.basename(currFilepath);
+console.log('basename -> : ',basename);
+
+
+
+// current file name without extention
+let basenameWithoutExt=path.basename(currFilepath,'.js');
+console.log('basenamewithoutExt -> : ',basenameWithoutExt);
+
+
+
+//directory name
+let dirname=path.dirname(currFilepath);
+console.log('dirname -> : ',dirname);
+
+console.log('ext -> : ',path.extname(currFilepath));
+console.log('ext -> : ',path.extname('index.md.js'));
+
+
+
+let pathTodir=path.join('/home','js','dist','index.js');
+console.log('pathToDir',pathTodir);
+console.log('parse -> : ',path.parse(currFilepath))
