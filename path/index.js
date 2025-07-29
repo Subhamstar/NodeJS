0d147d5ff359c0384faaefd25749ca2ddcd7682c
Subhamstar <mr.subhamdas999@gmail.com>
2025-07-29 13:47:34 +0530

    Removed node_modules and package-lock.json from tracking

---
+++ path/index.js
@@ -0,0 +1,9 @@
+const path=require("path");
+
+// console.log(path.sep);  //          \-windows, /-mac,linux
+
+
+// console.log(process.env.PATH);
+
+const currFilepath=__filename;
+console.log(currFilepath);
