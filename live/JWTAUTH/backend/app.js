c4c7dcabcde6d76a19a1e5a9ffaad6747bd0ee8c
Subhamstar <mr.subhamdas999@gmail.com>
2025-08-03 22:37:07 +0530

    jwt Auth done login and signup

--- live/JWTAUTH/backend/app.js
+++ live/JWTAUTH/backend/app.js
@@ -2,9 +2,14 @@ require('dotenv').config();
 const express=require('express');
 const app=express();
 const authRouter=require('./routes/authRoute.js')
-const connectToDB=require("../backend/config/databseConfig.js")
+const connectToDB=require("../backend/config/databseConfig.js");
+const cookieParser = require('cookie-parser');
 connectToDB();
+
+
 app.use(express.json());
+app.use(cookieParser);
+
 app.use("/api/auth",authRouter);// By default go to this authroutes and starting with /api/auth
 app.get("/",(req,res)=>{
     res.status(200).json({data:'JWTauth Server !!'});
