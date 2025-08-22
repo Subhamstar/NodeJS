025c8cf23f572b58abbe549f51fe5a2fc1c8a677
Subhamstar <mr.subhamdas999@gmail.com>
2025-08-22 22:37:29 +0530

    yt user and Video Schema Created

---
+++ BackendProject/src/db/user.models.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+const userSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique,
+        lowercase:true,
+        trim:true,
+        index:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique,
+        lowercase:true,
+        trim:true
+    },
+    fullname:{
+        type:String,
+        required:true,
+        lowercase:true,
+        trim:true,
+        index:true
+    },
+    avatar:{
+        type:String,
+        required:true
+    },
+    coverImage:{
+        type:String
+    },
+    watchHistory:
+    [   {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Video"
+        }
+    ],
+    password:{
+        type:String,
+        required:[true,"Password is required"]
+    },
+    refreshToken:{
+        type:String
+    }
+
+
+},{timestamps})
+
+
+
+
+export const User=mongoose.model("User",userSchema);
