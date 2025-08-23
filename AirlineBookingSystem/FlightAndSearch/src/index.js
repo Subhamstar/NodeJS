4cb5f1760ee0bd28caba85b61905c25e29ab6b1a
Subhamstar <mr.subhamdas999@gmail.com>
2025-08-23 19:32:14 +0530

    bodyparser

--- AirlineBookingSystem/FlightAndSearch/src/index.js
+++ AirlineBookingSystem/FlightAndSearch/src/index.js
@@ -1,9 +1,12 @@
-// const express=require("express");
-// import expressfrom'express'
+const express=require("express");
+const bodyParser=require("body-parser");
 require (dotenv).cangihdsmsy
 const setUpandStartServer=async()=>{
     const app=express();
     const PORT=3000;
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}))
+
     app.listen(PORT,()=>{
         console.log(`Server is listening on port ${PORT}`);
     })
