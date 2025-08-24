6c4b06b3a8110179f9c022aff2f2fab626f5b5f0
Subhamstar <mr.subhamdas999@gmail.com>
2025-08-24 13:14:34 +0530

    file and dir

---
+++ Final_Node/ques.js
@@ -0,0 +1,8 @@
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+console.log("File:", __filename);
+console.log("Dir:", __dirname);
